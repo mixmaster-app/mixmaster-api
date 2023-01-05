e61c90571316716d97479c800f447bbd737eb471
Kiio__ <kiio__@noreply.gitea.io>
2023-01-05 19:46:49 +0800

    Fix log rotation config

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -5,19 +5,21 @@ export const Config = {
         port: 3000,
         name: "mixmaster-api",
         description: "API of the game mixmaster <a href='https://mixmaster-online.fr'>https://mixmaster-online.fr</a>",
-        version: "1.0.0",
+        version: "1.4.1",
         openapi: "3.0.0",
     },
     Logger: {
         datePattern: "year-month-day hour:minute:second",
         doAppendToFile: true,
         file: {
-            path: "./var/logs",
+            path: "./",
             name: "mixapi.log",
             options: {
                 maxSize: "25M",
-                interval: "1d",
-                compress: "gzip"
+                maxFiles: 3,
+                interval: "7d",
+                compress: "gzip",
+                path: "./var/logs"
             }
         }
     },
